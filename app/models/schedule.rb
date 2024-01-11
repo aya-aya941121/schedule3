82ec1b3a3a71e618e2ec9182376158d1cae93aff
aya-aya941121 <94aya1121@gmail.com>
2024-01-11 15:28:18 +0900

    予定日時変更

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,5 +3,13 @@ class Schedule < ApplicationRecord
     validates :start_date, presence: true
     validates :end_date, presence: true
     validates :schedule_memo, length: { maximum: 500 }
-end
-# , date: { after_or_equal_to: :start_date }  
+    validate :end_date_after_start_date
+  
+    def end_date_after_start_date
+      return if end_date.blank? || start_date.blank?
+  
+      if end_date < start_date
+        errors.add(:end_date, "は開始日以降の日付を入力してください")
+      end
+    end
+  end
